f99ccb427865e12cee13c082781c46a6e0073f96
klkelley <kelley.15@gmail.com>
2017-10-11 18:04:04 -0500

    Add feature of user registration

--- spec/features/register_new_user_feature_spec.rb
+++ spec/features/register_new_user_feature_spec.rb
@@ -4,11 +4,12 @@ feature 'As a student I can sign up' do
   scenario 'A student successfully registers' do
     visit '/'
     click_link('sign up')
-    fill_in "user[username]", with: "Sammy"
+    # fill_in "user[username]", with: "Sammy"
     fill_in "user[email]", with: "sammy@cubs.com"
     fill_in "user[password]", with: "password"
-    click_button 'submit'
-    expect(page).to have_content 'Sammy'
+    fill_in 'user[password_confirmation]', with: "password"
+    click_button 'Sign up'
+    expect(page).to have_content 'Welcome! You have signed up successfully.'
   end
 
 
