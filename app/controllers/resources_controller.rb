5c6a705911207d415bc2554c35897204af0202c8
Gilad Shanan <email": null>
2017-10-12 11:35:10 -0500

    Add additional seed resources, add search results to index template, add search logic to index action

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,7 +1,17 @@
 class ResourcesController < ApplicationController
   def index
-    @videos = Resource.where(resource_type: "video").limit(3)
-    @snippets = Resource.where(resource_type: "snippet").limit(3)
-    @markdowns = Resource.where(resource_type: "markdown").limit(3)
+    if params[:term]
+      @resources = Resource.where('title LIKE ?', "%#{params[:term]}%")
+    else
+      @videos = Resource.where(resource_type: "video").limit(3)
+      @snippets = Resource.where(resource_type: "snippet").limit(3)
+      @markdowns = Resource.where(resource_type: "markdown").limit(3)
+    end
+  end
+
+  private
+
+  def resource_params
+    params.require(:resource).permit(:term)
   end
 end
--- spec/features/resource_index_feature_spec.rb
+++ spec/features/resource_index_feature_spec.rb
@@ -15,5 +15,21 @@ feature 'As a student I can view resources' do
     resource = Resource.find_by(title: "How to Build a Blog in Rails 4")
     expect(page).to have_current_path resource_path(resource)
   end
+end
 
+
+feature 'As a student, I can search resources' do
+  scenario 'The search field returns a match' do
+    visit '/resources'
+    fill_in "term", with: "Change Your Life"
+    click_button("Search")
+    expect(page).to have_content("How to Learn Rails and Change Your Life")
+  end
+
+  scenario "The search button doesn't change the page" do
+    visit '/resources'
+    fill_in "term", with: "Blog in Rails"
+    click_button("Search")
+    expect(current_path).to eq resources_path
+  end
 end
