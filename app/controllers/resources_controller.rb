bd380fb73b6aa89791d6bcd52b98a3acd4bdfd61
Gilad Shanan <email": null>
2017-10-12 10:43:21 -0500

    update resources index action, index page, show page

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -1,4 +1,7 @@
 class ResourcesController < ApplicationController
   def index
+    @videos = Resource.where(resource_type: "video").limit(3)
+    @snippets = Resource.where(resource_type: "snippet").limit(3)
+    @markdowns = Resource.where(resource_type: "markdown").limit(3)
   end
 end
--- spec/features/resource_index_feature_spec.rb
+++ spec/features/resource_index_feature_spec.rb
@@ -9,7 +9,7 @@ feature 'As a student I can view resources' do
     expect(page).to have_content 'Markdown'
   end
 
-  scenario 'A student visits the resource page' do
+  scenario 'A student clicks a link' do
     visit '/resources'
     click_link("How to Build a Blog in Rails 4")
     resource = Resource.find_by(title: "How to Build a Blog in Rails 4")
