8755f9f4598df251f25f2a4ba911c661d64f025d
Nicole Cody <nicole.cody7@gmail.com>
2017-10-12 19:41:33 -0500

    Add show functionality for markdown

--- app/assets/javascripts/snippet_show.js
+++ app/assets/javascripts/snippet_show.js
@@ -1,7 +1,18 @@
 
-var editor = ace.edit("editor");
+$(document).ready(function() {
+  console.log("HEREEEEEEEE")
+  var $target = $('#markdown')
+  var text = $target.text()
+  var converter = new showdown.Converter()
+  var html = converter.makeHtml(text);
+  console.log($target)
+  console.log(text)
+  console.log(html)
+    $target.html(html);
+})
+
+var editor = ace.edit("snippet");
 editor.setTheme("ace/theme/tomorrow_night");
 editor.getSession().setMode("ace/mode/ruby");
 // editor.getSession().setMode("ace/mode/javascript");
 editor.setReadOnly(true);
-
