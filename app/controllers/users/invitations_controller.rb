d89520b8940639b4c147b0209d72b12029313042
Nicole Cody <email": null>
2017-10-13 14:29:37 -0500

    update mailer config

--- app/controllers/users/invitations_controller.rb
+++ app/controllers/users/invitations_controller.rb
@@ -1,4 +1,8 @@
 class Users::InvitationsController < Devise::InvitationsController
   def new
+    p "InvitationsController"
+    @user = User.find(3)
+
+    TeacherMailer.sample_email(@user).deliver
   end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,6 +13,17 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
+  config.action_mailer.delivery_method = :smtp
+  # STMP settings for gmail
+  config.action_mailer.smtp_settings = {
+        :address          => "smtp.gmail.com",
+        :port             => 587,
+        :user_name        => ENV['gmail_username'],
+        :password         => ENV['gmail_password'],
+        :authentication   => "plain",
+    :enable_starttls_auto => true
+  }
+
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = true
 
