ae7d39145274b872b278c05d8646c619ab5b976e
Gilad Shanan <email": null>
2017-10-12 19:36:07 -0500

    update circleci seed file

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
 describe "Video virtual attributes" do
+  before (:all) do
+    Rails.application.load_seed
+  end
+
   let :video {Resource.find_by(resource_type: "video")}
   it "returns the url for a thumbnail image" do
     expect(video.thumb).to include "jpg"
