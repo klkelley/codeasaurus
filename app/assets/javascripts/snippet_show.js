a5972f61d352895ec63d40afada8fd1d85d57caf
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-10-13 13:46:06 -0500

    submit returns ace editor value

--- app/assets/javascripts/snippet_show.js
+++ app/assets/javascripts/snippet_show.js
@@ -1,17 +1,32 @@
 $(document).ready(function() {
+  var canWrite= !($("h2").text() === "Edit")
+  var $codeArea;
+  var editor;
   var $target = $('#markdown');
   var text = $target.text().trim()
   var converter = new showdown.Converter({omitExtraWLInCodeBlock: 'true'})
   var html = converter.makeHtml(text);
   $target.html(html);
-
   var $ace = $('#snippet');
+
+
+    $('.edit-field.snippet > .edit_resource').on("submit", function(){
+      event.preventDefault();
+      console.log(editor.getSession().getValue());
+      $codeArea.val(editor.getSession().getValue());
+    });
+
+
   if ($ace.size() > 0) {
-    var editor = ace.edit("snippet");
+    $codeArea = $( "[name='resource[body]']" )
+    editor = ace.edit("snippet");
     editor.setTheme("ace/theme/tomorrow_night");
     editor.getSession().setMode("ace/mode/ruby");
     // editor.getSession().setMode("ace/mode/javascript");
-    editor.setReadOnly(true);
+
+
+    editor.setReadOnly(canWrite);
+
   }
 
-})
+});
