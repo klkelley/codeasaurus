736f66b2aa35ce701c04d4e47e497ad0bc9e9687
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-10-13 15:30:41 -0500

    fix text

--- spec/features/edit_resource_feature_spec.rb
+++ spec/features/edit_resource_feature_spec.rb
@@ -6,7 +6,7 @@ feature 'A teacher can edit a code snippet' do
     edit_links = page.all('a', text: "edit")
     p edit_links
     # click_link(edit_links[4])
-    edit_links[4].click
+    edit_links[3].click
     expect(page).to have_selector("input[value='Validating Rails Models']")
   end
 end
