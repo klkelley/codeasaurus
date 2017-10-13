334566b8afc9347d79b649be04d17f8bbdebbf7f
Gilad Shanan <giladshanan@gmail.com>
2017-10-12 22:23:31 -0500

    fix deprecation warnings and ace errors

--- app/assets/javascripts/snippet_show.js
+++ app/assets/javascripts/snippet_show.js
@@ -4,10 +4,14 @@ $(document).ready(function() {
   var converter = new showdown.Converter()
   var html = converter.makeHtml(text);
     $target.html(html);
-})
 
-var editor = ace.edit("snippet");
-editor.setTheme("ace/theme/tomorrow_night");
-editor.getSession().setMode("ace/mode/ruby");
-// editor.getSession().setMode("ace/mode/javascript");
-editor.setReadOnly(true);
+  var $ace = $('#snippet')
+  if ($ace.size() > 0) {
+    var editor = ace.edit("snippet");
+    editor.setTheme("ace/theme/tomorrow_night");
+    editor.getSession().setMode("ace/mode/ruby");
+    // editor.getSession().setMode("ace/mode/javascript");
+    editor.setReadOnly(true);
+  }
+
+})
