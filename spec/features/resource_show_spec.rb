59111e497d43e9e9351150c250ffcc577318cace
Gilad Shanan <giladshanan@gmail.com>
2017-10-14 11:33:11 -0500

    update test suite and seed file

--- spec/features/resource_show_spec.rb
+++ spec/features/resource_show_spec.rb
@@ -1,6 +1,16 @@
 require 'rails_helper'
 
 feature 'As a student I can view snippets' do
+  before (:all) do
+    Rails.application.load_seed
+  end
+
+  before (:each) do
+    visit '/'
+    fill_in "user[email]", with: "sammy@student.com"
+    fill_in "user[password]", with: "gocubsgo"
+    click_button('Log in')
+  end
   scenario 'A student can view snippets' do
     visit '/resources'
     click_link("Validating Rails Models")
@@ -12,6 +22,13 @@ feature 'As a student I can view snippets' do
 end
 
 feature 'As a student I can view videos' do
+  before (:each) do
+    visit '/'
+    fill_in "user[email]", with: "sammy@student.com"
+    fill_in "user[password]", with: "gocubsgo"
+    click_button('Log in')
+  end
+
   scenario 'A student can access a video page' do
     visit '/resources'
     click_link("Ruby on Rails Tutorial")
