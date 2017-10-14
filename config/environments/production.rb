ab7a390b222c935b6550b75eff84e67edb8f4eae
Gilad Shanan <email": null>
2017-10-13 19:48:10 -0500

    host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,6 +15,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
 
+  config.action_mailer.default_url_options = { :host => 'codeasaurus.heroku.com' }
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
